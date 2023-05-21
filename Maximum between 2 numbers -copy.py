ed101a7953b53abe24da582706a1651cb36dde73
atul sisodiya <114668576+Atulpratapsisodiya7@users.noreply.github.com>
2023-05-21 16:44:11 +0530

    Create Maximum between 2 numbers -copy.py

---
+++ Maximum between 2 numbers -copy.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first nmuber"));
+b=int(input("enter the second number"));
+c=int(input("enter the third number"));
+if a>b and a>c:
+           print("max=",a);
+elif b>a and b>c:
+           print("max=",b);
+else:
+           print("max=",c);
+          
