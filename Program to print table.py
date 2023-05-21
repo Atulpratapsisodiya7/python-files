0c705d5a6f9b23f0c3d05f8824f95bfd4b811b56
atul sisodiya <114668576+Atulpratapsisodiya7@users.noreply.github.com>
2023-05-21 17:18:50 +0530

    Create Program to print table.py

---
+++ Program to print table.py
@@ -0,0 +1,6 @@
+# write a program to print table.
+a=int (input("enter the number"))
+b=int (input("enter the ending"))
+c=int (input("enter the icreasement"))
+for i in range(a,b+1,c):
+                print(i)
