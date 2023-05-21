4a9df872f8e1ddcbed2b6672630e7e4530088833
atul sisodiya <114668576+Atulpratapsisodiya7@users.noreply.github.com>
2023-05-21 17:17:54 +0530

    Create Program to print natural number from 1 to n.py

---
+++ Program to print natural number from 1 to n.py
@@ -0,0 +1,6 @@
+# write a program to print natural number from 1 to n.
+a=int (input("enter the number"))
+b=int (input("enter the ending"))
+c=int (input("enter the icreasement"))
+for i in range(a,b+1,c):
+                print(i)
