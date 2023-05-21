610939cf13304e60172c0d838492fa57cd383a4b
atul sisodiya <114668576+Atulpratapsisodiya7@users.noreply.github.com>
2023-05-21 17:07:14 +0530

    Create Program to find even number.py

---
+++ Program to find even number.py
@@ -0,0 +1,8 @@
+# write a program to find the sum of the square from 1 to n numbers.
+n=int(input("enter the number"))
+i=1
+sum=0
+while(i<=n):
+          sum=sum+i 
+          i=i+1         
+print("sum",sum)
