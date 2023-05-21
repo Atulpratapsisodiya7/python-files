777d9e546060335fdec679eb9ba5c759f7a0cf66
atul sisodiya <114668576+Atulpratapsisodiya7@users.noreply.github.com>
2023-05-21 17:21:23 +0530

    Create program to find scalene triangle or right or equilateral.py

---
+++ progrm to find scalene traingle or right or equilateral.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first side"))
+b=int(input("enter the second side"))
+c=int(input("enter the third side"))
+if a==b==c:
+                          print("equilateral traingle")
+elif a==b or b==c or a==c:
+                          print("isoscale triangle")
+else:
+                          print("scalene triangle") 
