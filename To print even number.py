7058b2e99bdeed8980114cdc12d0b9b7c5e5e89d
atul sisodiya <114668576+Atulpratapsisodiya7@users.noreply.github.com>
2023-05-21 17:44:04 +0530

    Create To print even number.py

---
+++ To print even number.py
@@ -0,0 +1,6 @@
+# to print even number from 1 to 10.
+a=int(input("enteer the number"))
+b=int(input("enter the ending"))
+c=int(input("enter the increament"))
+for i in range(a,b+1,c):
+    print(i)
