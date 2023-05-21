32d805fb6135ca36cc7d396247885bf872dcb14e
atul sisodiya <114668576+Atulpratapsisodiya7@users.noreply.github.com>
2023-05-21 17:44:40 +0530

    Create to check the number is postive or negative or zero - Copy.py

---
+++ to check the number is postive or negative or zero - Copy.py
@@ -0,0 +1,10 @@
+a=int(input("enter the first nmuber"));
+b=int(input("enter the second nmuber"));
+c=int(input("enter the third nmuber"));
+
+if a>b and a<c:
+           print("middle number",a);
+elif b>a and b<c:
+           print("middle number",b);
+else:
+           print("middle number",c);
